d9f2a2d870042f07b1c6c3e7bce76ca082372ba0
Gustavo Takeda <gustakeda@gmail.com>
2024-09-25 18:49:27 -0300

    Better basic scene and some assets

--- player_mov.gd
+++ player_mov.gd
@@ -3,6 +3,24 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const SENSITIVITY = 0.03
+
+const BOB_FREQ = 2.0
+const BOB_AMP = 0.08
+var t_bob = 0.0
+
+@onready var head = $Head
+@onready var cam = $Head/Camera3D
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		cam.rotate_x(-event.relative.y * SENSITIVITY)
+		cam.rotation.x = clamp(cam.rotation.x, deg_to_rad(-40), deg_to_rad(60))
 
 
 func _physics_process(delta: float) -> void:
@@ -11,13 +29,13 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir := Input.get_vector("left", "right", "foward", "backward")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -25,4 +43,15 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
+	# t_bob += delta * velocity.length() * float(is_on_floor())
+
+	# cam.transform.origin = _headbob(t_bob)
+
 	move_and_slide()
+
+
+# func _headbob(time) -> Vector3:
+# 	var pos = Vector3.ZERO
+# 	pos.y = sin(time * BOB_FREQ) * BOB_AMP
+# 	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
+# 	return pos
